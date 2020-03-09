2556785fbacab85f6f98025d9278bed8644981ed
Akira Ishino <akira.ishino@gmail.com>
2020-03-09 20:58:31 +0900

    Initial Search Feature

--- twitter_timeline_search/__init__.py
+++ twitter_timeline_search/__init__.py
@@ -9,6 +9,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'twitter.sqlite'),
+        SEARCH=os.path.join(app.instance_path, 'twitter.whoosh'),
     )
 
     if test_config is None:
@@ -26,6 +27,9 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    #from . import search
+    #search.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
---
+++ twitter_timeline_search/search.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+from whoosh.index import create_in, open_dir
+from whoosh.fields import Schema, KEYWORD, STORED
+from whoosh.qparser import QueryParser
+
+
+def get_search():
+    if 'search' not in g:
+        g.search = open_dir(current_app.config['SEARCH'])
+
+    return g.search
+
+
+def close_search(e=None):
+    pass
+
+
+def init_search():
+    schema = Schema(text=KEYWORD, id=STORED)
+    search_dir = current_app.config['SEARCH']
+    if os.path.exists(search_dir):
+        shutil.rmtree(search_dir)
+    os.mkdir(search_dir)
+    create_in(search_dir, schema)
+
+
+@click.command('init-search')
+@with_appcontext
+def init_search_command():
+    """Clear the existing index and create new search index."""
+    init_search()
+    click.echo('Initialized the search index.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_search)
+    app.cli.add_command(init_search_command)
+
+
+def add_tweet(writer, status):
+    writer.add_document(text=status.text, id=status.id_str)
+
+
+def search_tweets(query, limit=20):
+    ix = get_search()
+    with ix.searcher() as searcher:
+        parser = QueryParser("text", ix.schema)
+        q = parser.parse(query)
+        results = searcher.search(q, limit=limit)
+        return [r['id'] for r in results]
